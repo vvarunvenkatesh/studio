2ccd85bd1512954f52a99c7ca6eab230657173f5
varun Venkatesh <vanaparthivenkateshvarun@gmail.com>
2025-08-04 18:43:34 +0000

    and in mobile view advertising banner was fit to screen ,it is set to al

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -61,7 +61,7 @@ function AdvertisementSlider() {
   }, [goToNext]);
 
   return (
-    <div className="relative w-full h-72 md:h-96 lg:h-[28rem] overflow-hidden shadow-lg group">
+    <div className="relative w-full aspect-[1200/448] overflow-hidden shadow-lg group">
       {advertisements.map((ad, index) => (
         <Image
           key={ad.id}
